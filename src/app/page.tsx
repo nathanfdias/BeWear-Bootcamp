4946d3d003c685b4464c65ef0df6a14d2efabb29
nathanfdias <nathanfaria9@gmail.com>
2025-08-04 19:53:04 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
     <div>
-      <Button className="ny-1 bg-red-500 px-5">Bootcamp</Button>
+      <Button>Bootcamp</Button>
     </div>
   );
 };
