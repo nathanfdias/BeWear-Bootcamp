58a07ea41595d78a345eb48a34d36f2116f44b05
nathanfdias <nathanfaria9@gmail.com>
2025-08-05 12:27:12 -0300

    feat: add sign out

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,5 +1,6 @@
 // import { AppWindowIcon, CodeIcon } from "lucide-react"
 
+import { Header } from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignInForm from "./components/sign-in-form";
@@ -7,20 +8,23 @@ import SignUpForm from "./components/sign-up-form";
 
 const Authentication = async () => {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
-      <Tabs defaultValue="sign-in">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
-        </TabsList>
-        <TabsContent value="sign-in">
-          <SignInForm />
-        </TabsContent>
-        <TabsContent value="sign-up">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <Header />
+      <div className="flex w-full flex-col gap-6 p-5">
+        <Tabs defaultValue="sign-in">
+          <TabsList>
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="sign-in" className="w-full">
+            <SignInForm />
+          </TabsContent>
+          <TabsContent value="sign-up" className="w-full">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
